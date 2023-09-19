85bf9c7abe0aa47a2e777d8602add047da97ba7b
Jadan Kerr <badi1863@gmail.com>
2023-09-19 11:42:28 -0600

    Wrote some stuff, looks done! Need to add comments

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int size;
+        char fillChar;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the side-length of your equilateral triangle?");
+        size = scanner.nextInt();
+
+        System.out.println("What fill character would you like for your triangles?");
+        String nothing = scanner.nextLine();
+        fillChar = scanner.nextLine().charAt(0);
+        System.out.println("Here's your triangle: ");
+        triangleMaker(size, fillChar);
+
+    }
+    public static void triangleMaker(int size, char fillChar) {
+        if ((size > 50) || (size < 1)) {
+            System.out.println("I'm sorry your side-length is outside of my range (1-50). Please submit again:");
+        }
+        else{
+            for(int y = 0; y < size ; y++) {
+                System.out.printf("%"+((size-y) + 3)+"s", "   ");
+
+                for(int x = 0; x <= y; x++) {
+                    System.out.print(fillChar + " ");
+                }
+                System.out.print(" \n");
+            }
+        }
+    }
+}
